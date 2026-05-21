63e0eeb0acb9cb9ae85715890a35a74a51e4c0e5
Elaria Marzok <elariamarzok303@GMAIL.com>
2026-05-21 19:50:57 +0300

    refactor: comment out server listen code in server.js

--- Backend/server.js
+++ Backend/server.js
@@ -33,8 +33,8 @@ app.use('/cakes', cakeRoutes);
 app.use('/orders', orderRoutes);
 app.use('/auth', authRoutes);
 
-// Server listen
-const PORT = 4000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// // Server listen
+// const PORT = 4000;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
